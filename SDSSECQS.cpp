09ecd3475a8c36c2ee5198867414762e8abd67fa
shangqi <shangqi.lai@monash.edu>
2024-09-23 17:55:14 +1000

    Modify the program to accept test arguments

--- SDSSECQS.cpp
+++ SDSSECQS.cpp
@@ -4,24 +4,43 @@
 
 using namespace chrono;
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        cout << "Incorrect Arguments" << endl;
+        cout << "The program needs to be run with ./SDSSECQ [w1 size] [w2 size] [Deletion Size]" << endl;
+        return -1;
+    }
+
+    int w1_size = atoi(argv[1]);
+    int w2_size = atoi(argv[2]);
+    int del_size = atoi(argv[3]);
+
     SDSSECQSClient client;
 
-    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
-    for (int i = 0; i < 1000; ++i) {
+    auto start = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    for (int i = 0; i < w1_size; ++i) {
         client.update(INS, "alice", i);
+    }
+    for (int i = 0; i < w2_size; ++i) {
         client.update(INS, "bob", i);
     }
-    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
-    for (int i = 0; i < 100; ++i) {
+    auto end = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    cout << (float) (end - start) / (float) (w1_size + w2_size) << " us per insertion" << endl;
+    start = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    for (int i = 0; i < del_size; ++i) {
         client.update(DEL, "alice", i);
         client.update(DEL, "bob", i);
     }
-    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
-    vector<int> results = client.search(2, "alice", "bob");
-//    for (int res : results) {
-//        cout << res << endl;
-//    }
-    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
+    end = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    cout << (float) (end - start) / 2 / (float) del_size << " us per deletion" << endl;
+    // search the database
+    start = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    vector<int> single_results = client.search(1, "alice");
+    end = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    cout << "Single Keyword Search Time: " << (end - start) / 1000 << " ms" << endl;
+    start = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    vector<int> conjunctive_results = client.search(2, "alice", "bob");
+    end = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+    cout << "Two-Keyword Conjunctive Search Time: " << (end - start) / 1000 << " ms" << endl;
     return 0;
 }
